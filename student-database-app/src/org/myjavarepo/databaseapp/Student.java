6fc130c2b9316fe51f2249af18b9c2cf24a03be4
raquelceirao <raquel.ceirao@gmail.com>
2021-01-27 16:35:58 +0000

    Added student name and year prompt and set student ID

--- student-database-app/src/org/myjavarepo/databaseapp/Student.java
+++ student-database-app/src/org/myjavarepo/databaseapp/Student.java
@@ -1,18 +1,43 @@
 package org.myjavarepo.databaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 
     private String firstName;
     private String lastName;
-    private String gradeYear;
-    private int studentID;
+    private int gradeYear;
+    private String studentID;
     private String courses;
     private int tuitionBalance;
-    private int costOfCourse = 600;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
 
     //constructor to prompt name and year
+    public Student() {
+        Scanner in = new Scanner(System.in);
 
-    //generate an id
+        System.out.println("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("Enter student year: \n1) First Year Bachelor \n2) Second Year Bachelor \n3) Third year Bachelor \n4) Fourth Year Master \n5) Fifth Year Master");
+        this.gradeYear = in.nextInt();
+
+        setStudentID();
+
+        System.out.println(firstName + " " + lastName + ", Year " + gradeYear + " Student, ID " + studentID);
+
+    }
+
+    //generate an id = gradeYear + id
+    private void setStudentID() {
+        //everytime a student is created, increment 1 to the id
+        id++;
+        this.studentID = gradeYear + "" + id;
+    }
 
     //enroll in courses
 
--- student-database-app/src/org/myjavarepo/databaseapp/StudentDatabaseApp.java
+++ student-database-app/src/org/myjavarepo/databaseapp/StudentDatabaseApp.java
@@ -4,6 +4,8 @@ public class StudentDatabaseApp {
 
     public static void main(String[] args) {
 
+        Student gon = new Student();
+
         //ask how many new users we want to add
 
         //create n number of new students
